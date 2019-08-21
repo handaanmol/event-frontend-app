c3bceb7f85bd754a2e00a6111a22f11ead722b71
Handa <anmhanda@deloitte.com>
2019-08-21 15:14:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+function myCreateEvent() {
+var eventData =document.getElementById("feedDetail").value
+
+var url = "http://localhost:9002/event/apis/events"
+var query = "{\"eventName\":\"" + eventData + "\", \"date\":\""+ (new Date()).toString() +"\"}";
+console.log("query is", query);    
+const otherParams = {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json; charset=utf-8',
+        },
+        body: query
+    }
+    fetch(url, otherParams)
+        .then(data => {
+           console.log(data)
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .then(error => {
+            console.log(error);
+        })
+
+console.log("I am here")
+}
+
+
